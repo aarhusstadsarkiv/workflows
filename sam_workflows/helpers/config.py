85d01428c8e14fa88dbdd6237f85f45bd080b1bf
Claus Juhl Knudsen <clausjuhl@users.noreply.github.com>
2021-03-05 10:46:18 +0100

    Fixes #41. Config-file moved to Path.home() / '.sam_workflows' (#44)

--- sam_workflows/helpers/config.py
+++ sam_workflows/helpers/config.py
@@ -1,21 +1,28 @@
 import os
 import json
-from typing import List
+from typing import List, Dict
+from pathlib import Path
 
-from sam_workflows.settings import PACKAGE_PATH
+# from sam_workflows.settings import PACKAGE_PATH
 
 
-def load_config(config_keys: List[str]) -> None:
-    """Loads the passed config-keys in the os.environ, if they're found in
+def load_config(env_vars: List[str]) -> None:
+    """Loads the passed env_vars in the os.environ, if they're found in
     config.json.
 
     Parameters
     ----------
-    config_keys : List[str]
-        List of uppercase config-keys to load
+    env_vars : List[str]
+        List of environment variables to load
     """
-    with open(PACKAGE_PATH / "config.json") as c:
-        config = json.load(c)
-        for k, v in config.items():
-            if k.upper() in config_keys:
-                os.environ[k.upper()] = v
+    # with open(PACKAGE_PATH / "config.json") as c:
+    with open(Path.home() / ".sam_workflows" / "config.json") as c:
+        config: Dict = json.load(c)
+        for idx in env_vars:
+            env = idx.lower()
+            if env in config:
+                os.environ[env.upper()] = config[env]
+            else:
+                raise KeyError(
+                    f"Required environment variable not in config: {env}"
+                )
