16fa57462c023e56a41d919dc3425486ed0f7e86
Claus Juhl Knudsen <clausjuhl@users.noreply.github.com>
2021-05-25 10:05:08 +0200

    Version 0.4.1 (#90)
    * Fixing tif-problems. Midstream
    
    * Release 0.4.0
    
    * Typo-fixes
    
    * Set console to True in spec-file to make it work
    
    * Minor stuff
    
    * Reverting to console-generating version
    
    * Update version-number in main.py

--- sam_workflows/main.py
+++ sam_workflows/main.py
@@ -11,7 +11,7 @@ from sam_workflows.helpers import load_config
 # -----------------------------------------------------------------------------
 # Setup
 # -----------------------------------------------------------------------------
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 utf8_stdout = codecs.getwriter("utf-8")(sys.stdout.buffer, "strict")
 utf8_stderr = codecs.getwriter("utf-8")(sys.stderr.buffer, "strict")
@@ -19,6 +19,10 @@ if sys.stdout.encoding != "UTF-8":
     sys.stdout = utf8_stdout  # type: ignore
 if sys.stderr.encoding != "UTF-8":
     sys.stderr = utf8_stderr  # type: ignore
+
+# https://chriskiehl.com/article/packaging-gooey-with-pyinstaller
+# nonbuffered_stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+# sys.stdout = nonbuffered_stdout
 
 
 @Gooey(
