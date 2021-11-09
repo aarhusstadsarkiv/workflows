08e7b879134f57b8f18c3264ff38b1c1dee244e2
Claus Juhl Knudsen <cjk@aarhus.dk>
2021-11-09 12:23:35 +0100

    Remove 'source' directory

--- source/converters/__init__.py
+++
@@ -1,5 +0,0 @@
-import pdf as pdf_conv
-import video as video_conv
-import image as image_conv
-
-__all__ = ["pdf_conv", "video_conv", "image_conv"]
--- source/subcommands/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .images2pdf import images2pdf, ImageConvertError
-from .accessfiles import generate_sam_access_files
-
-__all__ = ["generate_sam_access_files"]
